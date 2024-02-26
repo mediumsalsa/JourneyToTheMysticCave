91e4d2ec589b1e1085e0720186d661bab96b6b1b
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2024-02-26 18:03:57 -0400

    Initial commit

---
+++ JourneyToTheMysticCave-Beta/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JourneyToTheMysticCave_Beta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# JourneyToTheMysticCave-Beta
+ 
